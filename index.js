5b0bb404338bfe12c52807aa4ac14cd2377e6ef6
Jordan Winslow <admin@jordanwinslow.me>
2019-07-02 05:07:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var animal = 'dog'
 
 function myAnimal() {
-  var animal = "cat"
+  // must be a dog
   return animal
 }
 
 function yourAnimal() {
+  var animal = "cat"
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
@@ -13,10 +14,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  return n + two
+  // Feel free to move things around!
+  
 }
 
 var funkyFunction = function() {
